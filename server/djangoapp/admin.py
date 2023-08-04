a45ac4a6984de56c563d15688bf882981552448d
Amehroke <arashdeepsingh123@gmail.com>
2023-08-04 14:13:00 -0400

    changes made from theia lab environment

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
-# from .models import related models
+from .models import CarMake, CarModel
 
+# Register your models here.
 
 # Register your models here.
 
 # CarModelInline class
-
+admin.site.register(CarModel)
+admin.site.register(CarMake)
 # CarModelAdmin class
 
 # CarMakeAdmin class with CarModelInline
