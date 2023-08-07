6ee8960e46f7af88b29a096a008463c0886165e6
Amehroke <arashdeepsingh123@gmail.com>
2023-08-07 02:24:38 -0400

    changes made from theia lab environment

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,23 +10,29 @@ urlpatterns = [
     # name the URL
 
     # path for about view
-    path('about', view=views.about, name='about'),
+    path(route='', view=views.get_dealerships, name='index'),
+    #week 1 T4
+    path(route='about', view=views.about, name='about'),
     # path for contact us view
-    path('contact', view=views.contact, name='contact'),
+    path(route='contact', view=views.contact, name='contact'),
 
     # path for registration
-    path('registration/', views.registration_request, name='registration'),
+    #week 2 t7
+    path('registration/', view=views.registration_request, name='registration'),
 
     # path for login
-    path('login/', views.login_request, name='login'),
-    # path for logout
-    path('logout/', views.logout_request, name='logout'),
+    #week 2 t 5
+    path(route='login/', view=views.login_request, name='login'),
 
-    path(route='', view=views.get_dealerships, name='index'),
+    # path for logout
+    #week 2 t 5
+    path(route='logout/', view=views.logout_request, name='logout'),
+
+    
 
     # path for dealer reviews view
     path('dealer/<int:dealer_id>/', view=views.get_dealer_details, name='dealer_details'),
     # path for add a review view
-    path('dealer/<int:id>/review', views.add_review, name='add_review'),
+    path(route='addreview/<int:dealer_id>/', view=views.add_review, name='add_review')
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
